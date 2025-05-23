2f19b0518e4b6ec7734b0abadc11ffc823f9fb78
Zach Bernal <chrisknyfe@gmail.com>
2025-05-23 01:30:23 -0700

    combine_duplicates second draft

--- scripts/combine_duplicates.py
+++ scripts/combine_duplicates.py
@@ -2,63 +2,95 @@ import sys
 import os
 import hashlib
 
+
+class FileStat:
+    def __init__(self, fullpath: str, rating: int):
+        self.fullpath = fullpath
+        self.rating = rating
+        self.size = os.path.getsize(fullpath)
+        self._md5sum_cached = None
+    def md5(self):
+        if not self._md5sum_cached:
+            self._md5sum_cached = hashlib.md5(open(self.fullpath,'rb').read()).hexdigest()
+        return self._md5sum_cached
+    def __repr__(self):
+        return f"FileStats('{self.fullpath}', {self.rating}, size: {self.size}, md5: {self._md5sum_cached})"
+    
+def list_filestats(inputdir: str):
+    filestats = []
+    for f in os.listdir(inputdir):
+        fullpath = os.path.join(inputdir, f)
+        if os.path.isdir(fullpath):
+            if not f.startswith("is_rated"):
+                continue
+            rating = int(f.split("_")[2])
+            print(f"rating is {rating} for dir {f}")
+            for f2 in os.listdir(fullpath):
+                fullpath2 = os.path.join(fullpath, f2)
+                if os.path.isfile(fullpath2):
+                    filestats.append(FileStat(fullpath2, rating))
+        elif os.path.isfile(fullpath):
+            rating = 1
+            filestats.append(FileStat(fullpath, rating))
+    return filestats
+
+
+def collect_files_by_size(filestats: list):
+    files_by_size = {}
+    for fs in filestats:
+        if not fs.size in files_by_size:
+            files_by_size[fs.size] = []
+        files_by_size[fs.size].append(fs)
+    return files_by_size
+
+
+def collect_files_by_md5(filestats: list):
+    files_by_md5 = {}
+    for fs in filestats:
+        if not fs.md5() in files_by_md5:
+            files_by_md5[fs.md5()] = []
+        files_by_md5[fs.md5()].append(fs)
+    return files_by_md5
+
+
+def combine_duplicates(filestats: list, inputdir: str):
+    if len(filestats) < 2:
+        return
+    print("has dupllicates:")
+    new_basename = os.path.basename(filestats[0].fullpath)
+    new_rating = 0
+    for fs in filestats:
+        b = os.path.basename(fs.fullpath)
+        if len(b) < len(new_basename):
+            new_basename = b
+        new_rating += fs.rating
+        print(f"\t{fs}")
+    new_fullpath = os.path.join(inputdir, f"is_rated_{new_rating}", new_basename)
+    print("new rating will be:")
+    print(f"\t{new_fullpath} rating: {new_rating}")
+
+    src = filestats[0].fullpath
+    dst = new_fullpath
+    if dry_run:
+        print(f"move {src} -> {dst}")
+    for fs in filestats[1:]:
+        if dry_run:
+            target = fs.fullpath
+            print(f"delete {target}")
+
+
+
 inputdir = sys.argv[1]
 
 dry_run = True
 
-# [{ fullpath, md5sum, rating }]
-file_ratings = []
+filestats = list_filestats(inputdir)
+files_by_size = collect_files_by_size(filestats)
+for size, files in files_by_size.items():
+    print(f"{size}: {files}")
+    if len(files) > 1:
+        files_by_md5 = collect_files_by_md5(files)
+        for md5, files2 in files_by_md5.items():
+            print(f"\t{md5}: {files2}")
+            combine_duplicates(files2, inputdir)
 
-for f in os.listdir(inputdir):
-    fullpath = os.path.join(inputdir, f)
-    
-    if os.path.isdir(fullpath):
-        if not f.startswith("is_rated"):
-            continue
-        rating = int(f.split("_")[2])
-        print(f"rating is {rating} for dir {f}")
-        for f2 in os.listdir(fullpath):
-            fullpath2 = os.path.join(fullpath, f2)
-            if os.path.isfile(fullpath2):
-                md5sum = hashlib.md5(open(fullpath2,'rb').read()).hexdigest()
-                size = os.path.getsize(fullpath2)
-                file_ratings.append({'fullpath': fullpath2, 'md5sum': md5sum, 'size': size, 'rating': rating})
-    elif os.path.isfile(fullpath):
-        rating = 1
-        md5sum = hashlib.md5(open(fullpath,'rb').read()).hexdigest()
-        size = os.path.getsize(fullpath)
-        file_ratings.append({'fullpath': fullpath, 'md5sum': md5sum, 'size': size, 'rating': rating})
-
-frcopy = file_ratings.copy()
-for rating in file_ratings:
-    print(rating)
-
-    duplicates = []
-    for i, r in reversed(list(enumerate(frcopy))):
-        if r['md5sum'] == rating['md5sum'] and r['size'] == rating['size']:
-            duplicates.append(r)
-            del frcopy[i]
-
-    # duplicates = [r for r in frcopy if r['md5sum'] == rating['md5sum'] and r['size'] == rating['size']]
-    if len(duplicates) > 1:
-        print("has dupllicates:")
-        new_basename = os.path.basename(duplicates[0]['fullpath'])
-        new_rating = 0
-        for d in duplicates:
-            b = os.path.basename(d['fullpath'])
-            if len(b) < len(new_basename):
-                new_basename = b
-            new_rating += d['rating']
-            print(f"\t{d}")
-        new_fullpath = os.path.join(inputdir, f"is_rated_{new_rating}", new_basename)
-        print("new rating will be:")
-        print(f"\t{new_fullpath} rating: {new_rating}")
-
-        src = duplicates[0]['fullpath']
-        dst = new_fullpath
-        if dry_run:
-            print(f"move {src} -> {dst}")
-        for d in duplicates[1:]:
-            if dry_run:
-                target = d['fullpath']
-                print(f"delete {target}")
